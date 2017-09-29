b6f2d6b9399ed52fee0a74848d9ca48e77a8a495
Jalektor <32129221+Jalektor@users.noreply.github.com>
2017-09-29 20:26:52 +0100

    Board creation

---
+++ TicTacToe/TicTacToe/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            board board = new board();
+
+            board.title();
+            board.createBoard();
+
+            Console.WriteLine("\nPlayer blah turn");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/board.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    // class designed to build the board and display messages
+    class board
+    {
+        public board()
+        {
+
+        }
+
+        
+        public void title()
+        {
+            Console.WriteLine("Welcome to Noughts & Crosses");
+            Console.WriteLine("Player one: x\nPLayer two: 0\n\n");
+        }
+        public void createBoard()
+        {
+            Console.WriteLine("[{}] [{}] [{}]");
+            Console.WriteLine("[{}] [{}] [{}]");
+            Console.WriteLine("[{}] [{}] [{}]");
+        }
+    }
+}
